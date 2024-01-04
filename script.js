78e5a257ce4351ce517f266ace525847618f2267
unknown <yonideserbaix@gmail.com>
2024-01-04 12:43:38 +0100

    update comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Récupérer les éléments HTML
+// Get HTML elements
 const inputField = document.getElementById("psw__input");
 const copyBtn = document.getElementById("psw__copy");
 const count = document.getElementById("Count");
@@ -6,45 +6,45 @@ const pswLength = document.getElementById("psw__length");
 const generateBtn = document.getElementById("psw__btn");
 
 copyPSW = () => {
-  // Sélectionner tout le texte dans le champ de texte du mot de passe
+  // Select all text in the password text field
   inputField.select();
 
-  // Plage de sélection pour les appareils mobiles
+  // Selection range for mobile devices
   inputField.setSelectionRange(0, 99999);
 
-  // Copie le texte dans le presse-papiers
+  // Copy the text to the clipboard
   navigator.clipboard.writeText(inputField.value);
 };
 
-// Fonction qui génère un mot de passe aléatoire en fonction de la longueur sélectionnée
+// Function to generate a random password based on the selected length
 generatePSW = () => {
-
   const characters =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
 
-  // Initialisation d'une chaîne vide pour le mot de passe généré
+  // Initialize an empty string for the generated password
   let password = "";
 
-  // Générer le mot de passe caractère par caractère
+  // Generate the password character by character
   for (let i = 0; i < pswLength.value; i++) {
-    // Générer un nombre aléatoire pour choisir un caractère
+    // Generate a random number to choose a character
     const randomNumber = Math.floor(Math.random() * characters.length);
 
-    // Ajouter le caractère sélectionné au mot de passe
+    // Add the selected character to the password
     password = password + characters[randomNumber];
   }
 
-  // Afficher le mot de passe généré dans le champ de texte du mot de passe
+  // Display the generated password in the password text field
   inputField.value = password;
 };
-// Fonction qui met à jour l'affichage de la longueur du mot de passe
+
+// Function to update the password length display
 const updatePswLength = (value) => {
-  // Afficher la longueur du mot de passe sélectionnée
+  // Display the selected password length
   count.textContent = value;
   generatePSW();
 };
 
-// Ajout d'un gestionnaire d'événements "input" à la plage de sélection
+// Add an "input" event listener to the length selection field
 pswLength.addEventListener("input", (e) => {
   const selectedLength = e.target.value;
   updatePswLength(selectedLength);
