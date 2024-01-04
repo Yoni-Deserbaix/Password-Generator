5ed649e9c4cf7fa367f690016a6fe1d4e1514dc1
unknown <yonideserbaix@gmail.com>
2024-01-04 12:32:38 +0100

    update variables name

--- script.js
+++ script.js
@@ -1,63 +1,61 @@
 // Récupérer les éléments HTML
-const champMotDePasse = document.getElementById("input");
-const boutonCopier = document.getElementById("copy__psw");
-const elementLongueurMotDePasse = document.getElementById("charCount");
-const plageLongueurMotDePasse = document.getElementById("length__psw");
-const boutonGenerer = document.getElementById("genere__psw");
+const inputField = document.getElementById("psw__input");
+const copyBtn = document.getElementById("psw__copy");
+const count = document.getElementById("Count");
+const pswLength = document.getElementById("psw__length");
+const generateBtn = document.getElementById("psw__btn");
 
 copyPSW = () => {
   // Sélectionner tout le texte dans le champ de texte du mot de passe
-  champMotDePasse.select();
+  inputField.select();
 
   // Plage de sélection pour les appareils mobiles
-  champMotDePasse.setSelectionRange(0, 99999);
+  inputField.setSelectionRange(0, 99999);
 
   // Copie le texte dans le presse-papiers
-  navigator.clipboard.writeText(champMotDePasse.value);
+  navigator.clipboard.writeText(inputField.value);
 };
 
 // Fonction qui génère un mot de passe aléatoire en fonction de la longueur sélectionnée
-genererMotDePasse = () => {
-  // Obtenir la longueur du mot de passe sélectionnée à partir de la plage de sélection
-  const longueurMotDePasse = plageLongueurMotDePasse.value;
+generatePSW = () => {
 
-  const caracteres =
+  const characters =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
 
   // Initialisation d'une chaîne vide pour le mot de passe généré
-  let motDePasse = "";
+  let password = "";
 
   // Générer le mot de passe caractère par caractère
-  for (let i = 0; i < longueurMotDePasse; i++) {
+  for (let i = 0; i < pswLength.value; i++) {
     // Générer un nombre aléatoire pour choisir un caractère
-    const nombreAleatoire = Math.floor(Math.random() * caracteres.length);
+    const randomNumber = Math.floor(Math.random() * characters.length);
 
     // Ajouter le caractère sélectionné au mot de passe
-    motDePasse = motDePasse + caracteres[nombreAleatoire];
+    password = password + characters[randomNumber];
   }
 
   // Afficher le mot de passe généré dans le champ de texte du mot de passe
-  champMotDePasse.value = motDePasse;
+  inputField.value = password;
 };
 // Fonction qui met à jour l'affichage de la longueur du mot de passe
-const mettreAJourLongueurMotDePasse = (valeur) => {
+const updatePswLength = (value) => {
   // Afficher la longueur du mot de passe sélectionnée
-  elementLongueurMotDePasse.textContent = valeur;
-  genererMotDePasse();
+  count.textContent = value;
+  generatePSW();
 };
 
 // Ajout d'un gestionnaire d'événements "input" à la plage de sélection
-plageLongueurMotDePasse.addEventListener("input", (e) => {
-  const longueurSelectionnee = e.target.value;
-  mettreAJourLongueurMotDePasse(longueurSelectionnee);
+pswLength.addEventListener("input", (e) => {
+  const selectedLength = e.target.value;
+  updatePswLength(selectedLength);
 });
 
-boutonCopier.addEventListener("click", () => {
+copyBtn.addEventListener("click", () => {
   event.preventDefault();
   copyPSW();
 });
 
-boutonGenerer.addEventListener("click", () => {
+generateBtn.addEventListener("click", () => {
   event.preventDefault();
-  genererMotDePasse();
+  generatePSW();
 });
